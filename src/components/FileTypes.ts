127cef61853fbf54085c7f044d5eeeafe20baee0
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-09-25 13:12:18 +0200

    Adds FileTypes enum

---
+++ src/components/FileTypes.ts
@@ -0,0 +1,10 @@
+export enum FileTypes {
+	mp4 = 'media/mp4' ,
+	mov = 'media/mov' ,
+	mkv = 'media/mkv' ,
+	gif = 'media/gif' ,
+	mp3 = 'audio/mp3' ,
+	aac = 'audio/aac' ,
+	wav = 'audio/wav' ,
+	ogg = 'audio/ogg' ,
+}
