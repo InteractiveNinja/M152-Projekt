918156fc95a52fe53e55e9f5d100b0b88208b239
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-09-25 13:35:27 +0200

    Adds loading spinner for conversion process

--- src/components/ConvertedFile.svelte
+++ src/components/ConvertedFile.svelte
@@ -2,17 +2,19 @@
 	import { onMount } from 'svelte';
 	import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 	import { FileTypes } from './FileTypes';
+	import LoadingSpinner from './LoadingSpinner.svelte';
 
 	export let file: File;
 	export let fileExt: FileTypes;
 	let convertedVideoUrl;
+	let loading = false;
 
 
 	const ffmpeg = createFFmpeg({ log: true });
 
 	async function convertFile(file: File, outputFileType: FileTypes) {
 		await ffmpeg.load();
-
+		loading = true;
 		const sourceFileExt = file.name.split('.').pop();
 		const inputFile = `input.${sourceFileExt}`;
 
@@ -21,6 +23,7 @@
 
 		ffmpeg.FS('writeFile', inputFile, await fetchFile(file));
 		await ffmpeg.run('-i', inputFile, outputFile);
+		loading = false;
 
 		const data = ffmpeg.FS('readFile', outputFile);
 
@@ -37,6 +40,10 @@
 <video controls>
 	<source src='{URL.createObjectURL(file)}' type='{file.type}'>
 </video>
+{#if loading}
+	<LoadingSpinner />
+	<span>Datei wird konvertiert...</span>
+	{/if}
 {#if convertedVideoUrl}
 	<p>Output Video</p>
 	{#if fileExt !== FileTypes.gif}
