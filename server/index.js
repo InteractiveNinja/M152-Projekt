6dc56350d55f95214903b372e22fac6ae6807729
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-09-25 14:07:33 +0200

    Again builds to node server instead to vercel
    Makes it easier to set headers instead through a service worker

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import { handler } from '../build/handler.js';
+import express from 'express';
+
+const app = express();
+
+// add a route that lives separately from the SvelteKit app
+app.get('/healthcheck', (req, res) => {
+	res.end('ok');
+});
+
+app.use(((req, res, next) => {
+	res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+	res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+	next();
+}))
+
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
+app.use(handler);
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+});
