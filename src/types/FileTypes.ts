e45743c43e0394b20c0a339c2fcdd03e4770dbfb
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-10-19 16:57:44 +0200

    Adds more supported Filetypes

--- src/types/FileTypes.ts
+++ src/types/FileTypes.ts
@@ -1,10 +1,14 @@
 export enum FileTypes {
-	mp4 = 'media/mp4',
-	mov = 'media/mov',
-	mkv = 'media/mkv',
-	gif = 'media/gif',
-	mp3 = 'audio/mp3',
-	aac = 'audio/aac',
-	wav = 'audio/wav',
-	ogg = 'audio/ogg'
+    mp4 = 'media/mp4',
+    avi = 'media/avi',
+    mov = 'media/mov',
+    mkv = 'media/mkv',
+    webm = 'media/webm',
+    gif = 'media/gif',
+    mp3 = 'audio/mp3',
+    aac = 'audio/aac',
+    wav = 'audio/wav',
+    flaac = 'audio/flaac',
+    ogg = 'audio/ogg',
+
 }
