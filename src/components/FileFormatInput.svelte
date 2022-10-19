5cf808a16b284604b666b299162974abeedda364
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-10-19 18:49:28 +0200

    formatting

--- src/components/FileFormatInput.svelte
+++ src/components/FileFormatInput.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-    import { FileTypes } from '../types/FileTypes';
-    import {createEventDispatcher} from "svelte";
+	import { FileTypes } from '../types/FileTypes';
+	import { createEventDispatcher } from 'svelte';
 
-    const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-    const fileFormatChoosen = (format: string) => {
-        dispatch("fileformat", {
-            format
-        })
-    }
-
+	const fileFormatChoosen = (format: string) => {
+		dispatch('fileformat', {
+			format
+		});
+	};
 </script>
+
 <select
-        class="form-select form-select-lg"
-        on:change={(e) => (fileFormatChoosen(e.target?.value))}
-        name="filetype"
-        id="filetype"
+	class="form-select form-select-lg"
+	on:change={(e) => fileFormatChoosen(e.target?.value)}
+	name="filetype"
+	id="filetype"
 >
-    <option disabled selected value>Formate</option>
-    {#each Object.entries(FileTypes) as [key, value]}
-        <option {value}>{key}</option>
-    {/each}
+	<option disabled selected value>Formate</option>
+	{#each Object.entries(FileTypes) as [key, value]}
+		<option {value}>{key}</option>
+	{/each}
 </select>
--- src/types/FileTypes.ts
+++ src/types/FileTypes.ts
@@ -1,14 +1,13 @@
 export enum FileTypes {
-    mp4 = 'media/mp4',
-    avi = 'media/avi',
-    mov = 'media/mov',
-    mkv = 'media/mkv',
-    webm = 'media/webm',
-    gif = 'media/gif',
-    mp3 = 'audio/mp3',
-    aac = 'audio/aac',
-    wav = 'audio/wav',
-    flaac = 'audio/flaac',
-    ogg = 'audio/ogg',
-
+	mp4 = 'media/mp4',
+	avi = 'media/avi',
+	mov = 'media/mov',
+	mkv = 'media/mkv',
+	webm = 'media/webm',
+	gif = 'media/gif',
+	mp3 = 'audio/mp3',
+	aac = 'audio/aac',
+	wav = 'audio/wav',
+	flaac = 'audio/flaac',
+	ogg = 'audio/ogg'
 }
