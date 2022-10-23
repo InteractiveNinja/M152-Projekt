5dc2f16e933bb7479922e1a67abfb4c2046e4c19
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-10-23 21:20:38 +0200

    updates responsive design for video showcase

--- src/components/VideoShowcase.svelte
+++ src/components/VideoShowcase.svelte
@@ -9,20 +9,40 @@
 	export let reset: () => void;
 </script>
 
-<div class="d-flex flex-column w-50">
+<div class="d-flex flex-column align-items-center">
 	{#if fileExt !== FileTypes.gif}
 		<!-- svelte-ignore a11y-media-has-caption -->
-		<video controls>
-			<source src={convertedVideoUrl} />
-		</video>
+		<div class="embed-responsive embed-responsive-16by9">
+			<video width="320" height="240" controls>
+				<source src={convertedVideoUrl} />
+			</video>
+		</div>
 	{:else}
-		<img src={convertedVideoUrl} type="media/gif" alt="Konvertierens GIF {convertedFileName}" />
+		<div class="embed-responsive embed-responsive-16by9">
+			<img
+				width="320"
+				height="240"
+				src={convertedVideoUrl}
+				alt="Konvertierens GIF {convertedFileName}"
+			/>
+		</div>
 	{/if}
-	<div class="d-flex justify-content-evenly p-1">
+	<div class="d-flex justify-content-evenly p-1 action-buttons">
 		<DownloadButton fileBlob={convertedVideoUrl} filename={convertedFileName} />
-		<Button variant="raised" color="secondary" on:click={reset}>
+		<Button class="retry-button" variant="raised" color="secondary" on:click={reset}>
 			<Icon class="material-icons">replay</Icon>
 			<Label>Nochmals Konvertieren</Label>
 		</Button>
 	</div>
 </div>
+
+<style lang="scss">
+	@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+		.action-buttons {
+			flex-direction: column;
+		}
+		:global(.retry-button) {
+			margin-top: 1em;
+		}
+	}
+</style>
